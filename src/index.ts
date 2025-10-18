afcd318cbd4730c53bee0692655949f792e102fd
firesquid6 <jdeiss06@gmail.com>
2025-10-18 17:13:48 -0500

    add the basic of a cli

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import { flag, option, positional, command } from "./argparse";
 
 const program = {
   sync: command({
-    flags: [],
+    flags: [
+      flag({ name: "no-remove", alternatives: ["K"] }),
+      flag({ name: "dry-run", alternatives: ["D"] }),
+      flag({ name: "only-dotfiles", alternatives: ["F"] }),
+    ],
     options: [],
     positional: [],
     subcommands: undefined,
@@ -11,4 +15,26 @@ const program = {
         
     },
   }),
+  update: command({
+    flags: [],
+    options: [],
+    positional: [
+      positional({ name: "pkg", required: false }),
+    ],
+    subcommands: undefined,
+    action(_options, _flags, positional) {
+      console.log("Update command is under construction");
+    },
+  }),
+  manage: command({
+    flags: [],
+    options: [],
+    positional: [],
+    subcommands: [
+    ],
+    action: (options, flags, positional) {
+      console.log("Manage command and subcommands still under construction!")
+    },
+  })
 }
+
