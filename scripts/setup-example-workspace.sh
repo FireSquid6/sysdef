1060bb49d55d919e2848e938597a4d826ad0f734
firesquid6 <jdeiss06@gmail.com>
2025-10-26 19:01:50 -0500

    fix the setup example workspace function

--- scripts/setup-example-workspace.sh
+++ scripts/setup-example-workspace.sh
@@ -2,5 +2,11 @@
 
 cd "$(dirname "$0")/.." || exit
 
+rm -rf ./example-workspace/sysdef-src
+rm -rf ./example-workspace/providers
+
 mkdir -p ./example-workspace/sysdef-src
 cp -r ./package/src/* ./example-workspace/sysdef-src
+
+mkdir -p ./example-workspace/providers
+cp ./providers/bun.ts ./example-workspace/providers/
