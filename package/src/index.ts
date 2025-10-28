1bc39b0e992836f00790bc61b3f966f14e374c7e
firesquid6 <jdeiss06@gmail.com>
2025-10-27 21:20:38 -0500

    remove argparse stuff

--- package/src/index.ts
+++ package/src/index.ts
@@ -6,69 +6,71 @@ import { Lockfile } from "./lockfile";
 import { dryFilesystem, normalFilesystem } from "./connections";
 import { syncModules } from "./sysdef";
 
-const program = {
-  _default: command({
-    flags: [],
-    options: [],
-    positional: [],
-    subcommands: undefined,
 
-    async action() {
-      console.log("This is your sysdef installation!");
-    },
-  }),
-  sync: command({
-    flags: [
-      flag({ name: "dry-run", alternatives: ["D"] }),
-    ],
-    options: [],
-    positional: [],
-    subcommands: undefined,
-    async action(options, flags, positional) {
-      // TODO - make the cli work properly
-      const dryRun: boolean = true;
-      const rootDir = path.join(os.homedir(), "sysdef");
+const program = command({
+  flags: [],
+  options: [],
+  positional: [],
 
-      const modules = await loadModules(rootDir, dryRun);
-      const providers = await loadProviders(rootDir, dryRun);
-      const store = await loadVariables(rootDir);
+  async action() {
+    console.log("This is your sysdef installation!");
+  },
+  subcommands: {
+    sync: command({
+      flags: [
+        flag({ name: "dry-run", alternatives: ["D"] }),
+      ],
+      options: [
+        option({ name: "mything", alternatives: ["-D"], required: true})
+      ],
+      positional: [],
+      subcommands: undefined,
+      async action(options, flags, positional) {
+        const dryRun: boolean = true;
+        // const rootDir = path.join(os.homedir(), "sysdef");
+        const rootDir = process.cwd();
 
-      const lockfile = new Lockfile();
-      lockfile.readFromFile(path.join(rootDir, "sysdef-lock.json"));
+        const modules = await loadModules(rootDir, dryRun);
+        const providers = await loadProviders(rootDir, dryRun);
+        const store = await loadVariables(rootDir);
 
-      const filesystem = dryRun ? dryFilesystem : normalFilesystem;
+        const lockfile = new Lockfile();
+        lockfile.readFromFile(path.join(rootDir, "sysdef-lock.json"));
 
-      await syncModules({
-        modules,
-        providers,
-        lockfile,
-        store,
-        filesystem
-      });
-    },
-  }),
-  update: command({
-    flags: [],
-    options: [],
-    positional: [
-      positional({ name: "pkg", required: false }),
-    ],
-    subcommands: undefined,
-    action(_options, _flags, positional) {
-      console.log("Update command is under construction");
-    },
-  }),
-  manage: command({
-    flags: [],
-    options: [],
-    positional: [],
-    subcommands: [
-    ],
-    action(options, flags, positional) {
-      console.log("Manage command and subcommands still under construction!")
-    },
-  })
-}
+        const filesystem = dryRun ? dryFilesystem : normalFilesystem;
+
+        await syncModules({
+          modules,
+          providers,
+          lockfile,
+          store,
+          filesystem
+        });
+      },
+    }),
+    update: command({
+      flags: [],
+      options: [],
+      positional: [
+        positional({ name: "pkg", required: false }),
+      ],
+      subcommands: undefined,
+      action(_options, _flags, positional) {
+        console.log("Update command is under construction");
+      },
+    }),
+    manage: command({
+      flags: [],
+      options: [],
+      positional: [],
+      subcommands: [
+      ],
+      action(options, flags, positional) {
+        console.log("Manage command and subcommands still under construction!")
+      },
+    })
+  },
+})
 
 
 // defaults to $HOME/sysdef. You could change this if you'd like! 
