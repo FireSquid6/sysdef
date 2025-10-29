f4e4e97e8f292aa5f48f923e4f552c1a393bd117
firesquid6 <jdeiss06@gmail.com>
2025-10-29 16:17:17 -0500

    add an example module

--- package/example-modules/core.ts
+++ package/example-modules/core.ts
@@ -5,11 +5,19 @@ import type { ModuleGenerator } from "../sysdef-src/sysdef";
 const m: ModuleGenerator = () => {
   return {
     name: "core",
+    variables: {
+      "HOME": "/home/me/"
+    },
+    files: {
+      // you can defined files that link to something locally:
+      "{HOME}/.config/example-config-file.txt": "./configfile.txt"
+    },
+    directories: {},
     packages: {
       // we can define the packages per provider in this
       "bun": [
         "@tailwindcss/cli",
-        ""
+        "typescript:5.9.3",
       ],
     }
   }
